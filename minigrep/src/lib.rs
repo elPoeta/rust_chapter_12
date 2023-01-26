341efd4fbbf52d6aec178fbddef8c04acf7afa93
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-01-26 07:04:32 -0300

    add case sentive search

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -22,12 +22,34 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
-
     Ok(())
+}
+
+fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "/ 
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search_case_sensitive(&query, &contents)
+        );
+    }
 }
